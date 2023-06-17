ee3d90a2bd9d23a67fa88f177c05884cb10233af
felipefigfaria <feeh.faria.98@gmail.com>
2023-06-17 17:01:37 -0300

    commit

---
+++ src/main/java/org/example/AberturaVenda.java
@@ -0,0 +1,18 @@
+package org.example;
+
+public class AberturaVenda implements  Tarefa {
+
+    private Venda venda;
+
+    public AberturaVenda(Venda venda){
+        this.venda = venda;
+    }
+
+    public void executar() {
+        this.venda.abrirVenda();
+    }
+
+    public void cancelar() {
+        this.venda.fecharVenda();
+    }
+}
---
+++ src/main/java/org/example/FecharVenda.java
@@ -0,0 +1,18 @@
+package org.example;
+
+public class FecharVenda implements  Tarefa {
+
+    private Venda venda;
+
+    public FecharVenda(Venda venda){
+        this.venda = venda;
+    }
+
+    public void executar() {
+        this.venda.fecharVenda();
+    }
+
+    public void cancelar() {
+        this.venda.abrirVenda();
+    }
+}
